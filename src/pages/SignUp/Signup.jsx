14740260aa1d63817cc4edb04ebc6297b42ee06c
Guilherme Lacerda <Guilherme.Gomes1@ibm.com>
2021-09-07 19:58:39 -0300

    Signup page

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,52 +1,156 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextInput,
   Tile,
   PasswordInput,
-  Link,
   Grid,
   Row,
   Column,
-  // Button,
+  InlineLoading,
+  Button,
 } from 'carbon-components-react';
-import { Link as RouterLink } from 'react-router-dom';
+import useSignup from './hooks/useSignup';
+import './signup.scss';
+// import { Link as RouterLink } from 'react-router-dom';
 
 export default function Login() {
-  return (
-    <Grid>
-      <Row>
-        <Column lg={{ span: 8, offset: 2 }}>
-          <h3 className="title">
-            Welcome to the HCI Papers Search and Analysis Platform!
-          </h3>
-        </Column>
-      </Row>
-      <Row>
-        <Column lg={{ span: 4, offset: 4 }}>
-          <Tile>
-            <h4 className="form-title">Login</h4>
-            <TextInput labelText="Username" />
-            <PasswordInput labelText="Password" />
-            <Link
-              className="recover-link"
-              href="#"
-              onClick={(e) => e.preventDefault()}
-            >
-              <RouterLink to="/recover">Forgot password?</RouterLink>
-            </Link>
-            {/* <Button kind="primary">Log in</Button> */}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [validatingUser, setValidatingUser] = useState(false);
+  const [invalidInputs, setInvalidInputs] = useState({
+    firstName: false,
+    lastName: false,
+    password: false,
+    email: false,
+  });
 
-            <Link
-              className="new-account-link"
-              href="#"
-              onClick={(e) => e.preventDefault()}
-            >
-              <RouterLink to="/recover">Create new account</RouterLink>
-            </Link>
-          </Tile>
-        </Column>
-      </Row>
-    </Grid>
+  const { signup } = useSignup();
+
+  /**
+   *
+   * Checks if all inputs are valid. If so, returns true. If not, returns false.
+   */
+  function validateInputs() {
+    let invalidEmail = false;
+    let invalidPassword = false;
+    let invalidFirstName = false;
+    let invalidLastName = false;
+
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email.toLowerCase()
+      )
+    ) {
+      invalidEmail = true;
+    }
+
+    if (password.length <= 0) {
+      invalidPassword = true;
+    }
+
+    if (firstName.length <= 0) {
+      invalidFirstName = true;
+    }
+
+    if (lastName.length <= 0) {
+      invalidLastName = true;
+    }
+
+    setInvalidInputs({
+      password: invalidPassword,
+      email: invalidEmail,
+      firstName: invalidFirstName,
+      lastName: invalidLastName,
+    });
+
+    if (
+      invalidEmail ||
+      invalidFirstName ||
+      invalidLastName ||
+      invalidPassword
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  function validateAndSignup() {
+    if (!validateInputs()) {
+      return;
+    }
+    signup();
+    setValidatingUser(true);
+  }
+
+  return (
+    <div className="background-gradient">
+      <Grid>
+        <Row>
+          <Column lg={{ span: 8, offset: 2 }}>
+            <h3 className="title">
+              Welcome to the HCI Papers Search and Analysis Platform!
+            </h3>
+          </Column>
+        </Row>
+        <Row>
+          <Column lg={{ span: 4, offset: 4 }}>
+            <Tile className="tile">
+              <h4 className="form-title">Sign Up</h4>
+              <div className="input">
+                <TextInput
+                  labelText="First Name"
+                  invalid={invalidInputs.firstName}
+                  invalidText="Please type your name."
+                  maxLength="20"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <TextInput
+                  labelText="Last Name"
+                  invalid={invalidInputs.lastName}
+                  invalidText="Please type your last name."
+                  maxLength="20"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <TextInput
+                  labelText="Email"
+                  invalid={invalidInputs.email}
+                  invalidText="Please type your email correctly."
+                  maxLength="30"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <PasswordInput
+                  labelText="Password"
+                  invalid={invalidInputs.password}
+                  invalidText="Please type your password."
+                  maxLength="20"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {validatingUser ? (
+                <InlineLoading />
+              ) : (
+                <Button
+                  size="small"
+                  className="login-button"
+                  onClick={validateAndSignup}
+                >
+                  Register
+                </Button>
+              )}
+            </Tile>
+          </Column>
+        </Row>
+      </Grid>
+    </div>
   );
 }
---
+++ src/pages/SignUp/hooks/useSignup.js
@@ -0,0 +1,6 @@
+export default function useSignup() {
+  function signup() {
+    return 1;
+  }
+  return { signup };
+}
