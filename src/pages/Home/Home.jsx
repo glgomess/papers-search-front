b039b9dffc2e43cf0308fdd1ab24ae00470641be
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-10-16 19:01:44 -0300

    Rendering list dynamically

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Grid } from 'carbon-components-react';
-import { Content } from 'carbon-components-react/lib/components/UIShell';
-import {Header} from '../../components/index';
-import ArticlesFilter from './ArticlesFilter';
+import React, { useState } from "react";
+import { Grid } from "carbon-components-react";
+import { Content } from "carbon-components-react/lib/components/UIShell";
+import { Article, Header } from "../../components/index";
+import ArticlesFilter from "./ArticlesFilter";
+import Skeleton from "./Skeleton";
+import "./home.scss";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Content>
-        <Grid>
-          <ArticlesFilter />
-        </Grid>
-      </Content>
-    </>
-  );
+	const [articles, setArticles] = useState([]);
+	const [isSearching, setIsSearching] = useState(false);
+
+	return (
+		<>
+			<Header />
+			<Content>
+				<Grid>
+					<ArticlesFilter
+						updateArticleList={setArticles}
+						updateIsSearching={setIsSearching}
+					/>
+					<div className="article-list">
+						{articles.map((art) => (
+							<Article article={art} />
+						))}
+					</div>
+					{isSearching ? <Skeleton /> : null}
+				</Grid>
+			</Content>
+		</>
+	);
 }
