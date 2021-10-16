baf0f4ee8ffb7fc6cfef3225fc82628b48ee3077
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-10-16 19:01:34 -0300

    Logic to render commas correctly

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import "./article.scss";
 
-const exampleArticle = {
-	authors: ["Piccolo, Lara Schibelsky Godoy", "Baranauskas, Maria Cecília C."],
-	bookTitle:
-		"Proceedings of VII Brazilian Symposium on Human Factors in Computing Systems",
-	abstract:
-		"Taking in account the social relevance of the terrestrial TV in Brazil and the transition to the digital technology - meaning new opportunities for exploring interactivity on TV - this paper is a worldwide review of HCI studies applied to interactive TV. Organizational semiotics artifacts are used in order to identify the main questions related to the interactive TV in the Brazil scene. The main design challenges are also pointed out by this study.",
-	keywords: ["human computer interaction", "digital interactive tv"],
-};
-export default function Article() {
+export default function Article({ article }) {
 	return (
 		<div className="article-container">
-			<span className="article-title">{exampleArticle.bookTitle}</span>
+			<span className="article-title">{article.bookTitle}</span>
 			<div className="article-subtopic">
 				<span className="article-subtitle">Abastract: </span>
-				<span className="article-subtitle-text">{exampleArticle.abstract}</span>
+				<span className="article-subtitle-text">{article.abstract}</span>
 			</div>
 			<div className="article-subtopic">
 				<span className="article-subtitle">Authors: </span>
 				<span className="article-subtitle-text">
-					{exampleArticle.authors.map((kw, index) =>
-						index + 1 === exampleArticle.authors.length ? `${kw}` : `${kw}, `
+					{article.authors.map((kw, index) =>
+						index + 1 === article.authors.length ? `${kw}` : `${kw}, `
 					)}
 				</span>
 			</div>
 			<div className="article-subtopic">
 				<span className="article-subtitle">Keywords: </span>
 				<span className="article-subtitle-text">
-					{exampleArticle.keywords.map((kw) => `${kw}, `)}
+					{article.keywords.map((kw) => `${kw}, `)}
 				</span>
 			</div>
 		</div>
