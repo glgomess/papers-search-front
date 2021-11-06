8ebd6ace913b3da7b6b6c4f1a7eddd57c3720f55
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-11-06 17:27:06 -0300

    Reading list component

---
+++ src/components/ReadingList/ReadingList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Modal } from "carbon-components-react";
+import Article from "../Article";
+import "./readingList.scss";
+
+export default function ReadingList({
+	articles,
+	openReadingList,
+	removeFromReadingList,
+	onCloseReadingList,
+}) {
+	return ReactDOM.createPortal(
+		<Modal
+			modalHeading="Reading List"
+			className="reading-list"
+			passiveModal
+			open={openReadingList}
+			onRequestClose={onCloseReadingList}
+			size="lg"
+		>
+			<>
+				{articles.length > 0 ? (
+					articles.map((art) => (
+						<Article
+							article={art}
+							isInReadingList
+							removeFromReadingList={removeFromReadingList}
+						/>
+					))
+				) : (
+					<span className="empty-list">
+						No articles currently in your list.
+					</span>
+				)}
+			</>
+		</Modal>,
+		// eslint-disable-next-line no-undef
+		document.body
+	);
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Suggestions from "./Suggestions";
 import Article from "./Article";
+import ReadingList from "./ReadingList/ReadingList";
 
-export { Header, Suggestions, Article };
+export { Header, Suggestions, Article, ReadingList };
