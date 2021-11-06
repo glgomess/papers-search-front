15191ef46f254e44e311e13df87d893dd6d518f8
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-11-06 19:23:51 -0300

    Separating functions by author and keywords

--- src/pages/Home/hooks/useSearch.js
+++ src/pages/Home/hooks/useSearch.js
@@ -9,11 +9,19 @@ export default function useSearch() {
 		return response.data;
 	}
 
-	async function searchArticles(filters, matchAll) {
+	async function searchAuthors(word) {
+		const response = await backendApi().get(
+			`/elastic/search/authors?author=${word}`
+		);
+
+		return response.data;
+	}
+
+	async function searchArticles(keywordsFiltered, authorsFiltered, matchAll) {
 		const response = await backendApi().get(
 			`/elastic/search/articles?keywords=${encodeURIComponent(
-				filters
-			)}&matchAll=${matchAll}`
+				keywordsFiltered
+			)}&authors=${encodeURIComponent(authorsFiltered)}&matchAll=${matchAll}`
 		);
 
 		return response.data;
@@ -22,5 +30,6 @@ export default function useSearch() {
 	return {
 		searchKeywords,
 		searchArticles,
+		searchAuthors,
 	};
 }
